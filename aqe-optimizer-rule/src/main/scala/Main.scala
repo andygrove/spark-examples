4663201b2f359e083451b0d4c206d0a9f1b61e6d
Andy Grove <andygrove@nvidia.com>
2022-07-07 17:04:38 -0600

    save

--- aqe-optimizer-rule/src/main/scala/Main.scala
+++ aqe-optimizer-rule/src/main/scala/Main.scala
@@ -8,7 +8,7 @@ import org.apache.spark.sql.catalyst.plans.logical.LogicalPlan
 import org.apache.spark.sql.catalyst.rules.Rule
 import org.apache.spark.sql.execution.SparkPlan
 import org.apache.spark.sql.execution.adaptive.{AdaptiveSparkPlanExec, BroadcastQueryStageExec, LogicalQueryStage, ShuffleQueryStageExec}
-import org.apache.spark.sql.internal.StaticSQLConf
+import org.apache.spark.sql.internal.{SQLConf, StaticSQLConf}
 import org.rogach.scallop._
 
 import scala.collection.mutable.ListBuffer
@@ -23,17 +23,58 @@ object Main {
   def main(arg: Array[String]) {
 //    val conf = new Conf(arg)
 
+    val enableCbo = true
+
     val spark = SparkSession.builder()
       .master("local[*]")
       .config("spark.plugins", "MySparkPlugin")
+      .config(SQLConf.CBO_ENABLED.key, s"$enableCbo")
+      .config(SQLConf.PLAN_STATS_ENABLED.key, s"$enableCbo")
       .config(StaticSQLConf.SPARK_SESSION_EXTENSIONS.key, "MyExtensions")
       .getOrCreate()
 
     import spark.implicits._
 
-    val tables = Seq("customer", "lineitem", "nation", "orders", "part", "partsupp", "region", "supplier")
+//    val tables = Seq("customer", "lineitem", "nation", "orders", "part", "partsupp", "region", "supplier")
+    val tables = Seq("customer", "orders", "lineitem")
 
-    tables.foreach(t => spark.read.parquet(s"/mnt/bigdata/tpch/sf1-parquet/$t").createOrReplaceTempView(t))
+    if (enableCbo) {
+      tables.foreach { t =>
+        spark.sql(s"CREATE TABLE $t USING parquet LOCATION '/mnt/bigdata/tpch/sf1-parquet/$t'").show()
+        spark.sql(s"ANALYZE TABLE $t COMPUTE STATISTICS").show()
+        spark.sql(s"DESC EXTENDED $t").show()
+
+        /*
+        +--------------------+--------------------+-------+
+        |            col_name|           data_type|comment|
+        +--------------------+--------------------+-------+
+        |          o_orderkey|              bigint|   null|
+        |           o_custkey|              bigint|   null|
+        |       o_orderstatus|              string|   null|
+        |        o_totalprice|              double|   null|
+        |         o_orderdate|                date|   null|
+        |     o_orderpriority|              string|   null|
+        |             o_clerk|              string|   null|
+        |      o_shippriority|                 int|   null|
+        |           o_comment|              string|   null|
+        |                    |                    |       |
+        |# Detailed Table ...|                    |       |
+        |             Catalog|       spark_catalog|       |
+        |            Database|             default|       |
+        |               Table|              orders|       |
+        |        Created Time|Thu Jul 07 16:40:...|       |
+        |         Last Access|             UNKNOWN|       |
+        |          Created By|Spark 3.4.0-SNAPSHOT|       |
+        |                Type|            EXTERNAL|       |
+        |            Provider|             parquet|       |
+        |          Statistics|451389455 bytes, ...|       |
+        +--------------------+--------------------+-------+
+        */
+      }
+    } else {
+      // register temp tables
+      tables.foreach(t => spark.read.parquet(s"/mnt/bigdata/tpch/sf1-parquet/$t").createOrReplaceTempView(t))
+    }
 
     val df = spark.sql("""select
                          |    l_orderkey,
@@ -93,12 +134,15 @@ class MyRule(sparkSession: SparkSession) extends Rule[LogicalPlan] {
       .map(_.asInstanceOf[LogicalQueryStage])
 
     for (stage <- stages) {
-      println("computeStats: " + stage.computeStats())
+      val stats = stage.computeStats()
+      println("computeStats: " + stats)
       /*
       computeStats: Statistics(sizeInBytes=4.6 MiB, rowCount=3.00E+5)
       computeStats: Statistics(sizeInBytes=114.8 MiB)
       computeStats: Statistics(sizeInBytes=308.8 MiB)
        */
+
+      println(stats.attributeStats.map(c => c.toString()))
     }
 
     plan
@@ -113,6 +157,7 @@ class MyRule(sparkSession: SparkSession) extends Rule[LogicalPlan] {
         case _ if predicate(plan) =>
           accum += plan
           plan.children.flatMap(p => recurse(p, predicate, accum)).headOption
+        // treat LogicalQueryStage as leaf node and do not recurse
         //case a: LogicalQueryStage => recurse(a.logicalPlan, predicate, accum)
         case other => other.children.flatMap(p => recurse(p, predicate, accum)).headOption
       }
